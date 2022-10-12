41e6c2a3772ae0977ea4118820309959cf8c751d
Ross <rosskeene196@gmail.com>
2022-10-12 16:59:38 -0500

    nodemon installed

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,14 +1,52 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', (reg, res, next) => {
+router.get('/', (req, res, next) => {
     res.status(200).json({
    message: "Handling GET request of the /users"
     });
+   
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+   message: "Handling POST request of the /users"
+    });
+   
+});
+
+router.get('/:userID', (req, res, next) => {
+const id = req.params.userID;
+if (id === 'admin'){
+    res.status(200).json({
+        message: 'You are the Admin!',
+        ID: id
+    });
+} else {
+    res.status(200).json({
+     message: 'You are a standard user',
+     ID: id
+    });
+}
+
 });
 
 
+router.patch('/:userID', (req, res, next) => {
+    const id= req.params.userID;
+    // find and update user by id
+    res.status(200).json({
+        message: 'User Updated!'
+    });
+});
 
+router.delete('/:userID', (req, res, next) => {
+    const id= req.params.userID;
+    // find and update user by id
+    res.status(200).json({
+        message: 'User Deleted!'
+    });
+});
 
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const usersRouters = require('./api/routes/users')
 
+
+app.use('/users', usersRouters);
 
 app.use((req, res, next) => {
     res.status(200).json({
@@ -8,8 +11,7 @@ app.use((req, res, next) => {
     });
 });
 
-const usersRouters = require('.api/routes/users')
-app.use('/users', usersRouters);
+
 
 
 
